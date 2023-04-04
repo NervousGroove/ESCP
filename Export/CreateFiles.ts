3f9876aa4857d66af2ec60aaeea76e63026978c7
SwankyNoob <102262759+NervousGroove@users.noreply.github.com>
2023-04-04 17:11:15 -0300

    Update CreateFiles.ts

--- Export/CreateFiles.ts
+++ Export/CreateFiles.ts
@@ -1,6 +1,6 @@
 function ExportSpikeCode() {
   const fs = require('fs');
-  const inputText = document.getElementById('blablabla').textContent;
+  const inputText = document.getElementByClassName('inputarea monaco-mouse-cursor-text').textContent;
   const filePath = 'C:/SpellSpikeLang/main.spike';
   fs.writeFileSync(filePath, inputText);
 }
